6f7583fb3faf1af8823e974ade02ece0a908812f
DevHN504 <155341765+DevHN504@users.noreply.github.com>
2024-03-30 14:47:00 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Npgsql;
+
+string connectionString = "Server=YourServerName.amazonaws.com;Port=YourPort;Database=YourDatabase;User Id=YourUser;Password='YourPassword';";
+
+//Establish Connection
+//await using var connection = new NpgsqlConnection(connectionString);
+//await connection.OpenAsync();
+//Console.WriteLine("Connected");
+//await connection.CloseAsync();
+
+//Execute SQL Queries
+await using var connection = new NpgsqlConnection(connectionString);
+await connection.OpenAsync();
+
+string query = "SELECT * FROM my_table";
+await using (var cmd = new NpgsqlCommand(query, connection))
+await using (var reader = await cmd.ExecuteReaderAsync())
+{
+    while (await reader.ReadAsync())
+    {
+        //Do whatever you need
+        Console.WriteLine($"{reader.GetString(0)} {reader.GetString(1)}");
+    }
+}
+
+await connection.CloseAsync();
